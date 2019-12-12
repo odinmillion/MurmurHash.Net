9bf6f7c22e0577c2fdb550b03504331e1007b227
Evgeny Yuryev <eyuryev@ozon.ru>
2019-12-12 19:38:24 +0300

    fix bugs

--- src/MurmurHash.Net/MurmurHash3.cs
+++ src/MurmurHash.Net/MurmurHash3.cs
@@ -1,55 +1,56 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace MurmurHash.Net
 {
     public class MurmurHash3
     {
-        public static uint Hash(Span<byte> bytes, uint seed)
+        public static uint Hash32(Span<byte> bytes, uint seed)
         {
-            uint num1 = seed;
-            int start;
-            for (start = 0; start < bytes.Length - 4; start += 4)
-            {
-                Span<byte> span2 = bytes.Slice(start, 4);
-                uint num2 =
-                    Rotl32((uint) ((int) span2[0] | (int) span2[1] << 8 | (int) span2[2] << 16 | (int) span2[3] << 24) * 3432918353U,
-                        (byte) 15) * 461845907U;
-                num1 = (uint) ((int) Rotl32(num1 ^ num2, (byte) 13) * 5 - 430675100);
-            }
+            var length = bytes.Length;
+            var h1 = seed;
+            var remainder = length & 3;
+            var position = length - remainder;
+            for (var start = 0; start < position; start += 4)
+                h1 = (uint) ((int) RotateLeft(h1 ^ RotateLeft(BitConverter.ToUInt32(bytes.Slice(start, 4)) * 3432918353U,15) * 461845907U, 13) * 5 - 430675100);
 
-            if (start < bytes.Length)
+            if (remainder > 0)
             {
-                Span<byte> span2 = bytes.Slice(start, bytes.Length - start);
-                switch (span2.Length)
+                uint num = 0;
+                switch (remainder)
                 {
                     case 1:
-                        uint num2 = Rotl32((uint) span2[0] * 3432918353U, (byte) 15) * 461845907U;
-                        num1 ^= num2;
+                        num ^= (uint) bytes[position];
                         break;
                     case 2:
-                        uint num3 = Rotl32(((uint) span2[0] | (uint) span2[1] << 8) * 3432918353U, (byte) 15) * 461845907U;
-                        num1 ^= num3;
-                        break;
+                        num ^= (uint) bytes[position + 1] << 8;
+                        goto case 1;
                     case 3:
-                        uint num4 = Rotl32((uint) ((int) span2[0] | (int) span2[1] << 8 | (int) span2[2] << 16) * 3432918353U, (byte) 15) *
-                                    461845907U;
-                        num1 ^= num4;
-                        break;
+                        num ^= (uint) bytes[position + 2] << 16;
+                        goto case 2;
                 }
+
+                h1 ^= RotateLeft(num * 3432918353U, 15) * 461845907U;
             }
+            
+            h1 = FMix(h1 ^ (uint) length);
 
-            uint h = num1 ^ (uint) bytes.Length;
-            h ^= h >> 16;
-            h *= 2246822507U;
-            h ^= h >> 13;
-            h *= 3266489909U;
-            h ^= h >> 16;
-            return h;
+            return h1;
         }
-
-        private static uint Rotl32(uint x, byte r)
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static uint RotateLeft(uint x, byte r)
         {
             return x << (int) r | x >> 32 - (int) r;
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static uint FMix(uint h)
+        {
+            h = (uint) (((int) h ^ (int) (h >> 16)) * -2048144789);
+            h = (uint) (((int) h ^ (int) (h >> 13)) * -1028477387);
+            return h ^ h >> 16;
+        }
     }
+
 }
--- tests/MurmurHash.Net.Tests/MurmurHashTests.cs
+++ tests/MurmurHash.Net.Tests/MurmurHashTests.cs
@@ -1,15 +1,33 @@
-﻿using System.Linq;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Xunit;
 
 namespace MurmurHash.Net.Tests
 {
     public class MurmurHashTests
     {
-        [Fact]
-        public void SmokeTest()
+        [Theory]
+        [InlineData(new byte[] {1, 2, 3}, 293U, 1971362553U)]
+        [InlineData(new byte[] {1, 2, 3, 4}, 293U, 2911303516U)]
+        [InlineData(new byte[] {1, 2, 3, 4, 5}, 293U, 3144920404U)]
+        [InlineData(new byte[] {1, 2, 3, 4, 5, 6}, 293U, 3796703664U)]
+        [InlineData(new byte[] {1, 2, 3, 4, 5, 6, 7}, 293U, 3433363787U)]
+        [InlineData(new byte[] {1, 2, 3, 4, 5, 6, 7, 8}, 293U, 2343089733U)]
+        [InlineData(new byte[0], 0U, 0U)]
+        [InlineData(new byte[0], 1U, 0x514E28B7U)]
+        [InlineData(new byte[0], 0xffffffffU, 0x81F16F39U)]
+        [InlineData(new byte[] {0xFF, 0xFF, 0xFF, 0xFF}, 0U, 0x76293B50U)]
+        [InlineData(new byte[] {0x21, 0x43, 0x65, 0x87}, 0U, 0xF55B516BU)]
+        [InlineData(new byte[] {0x21, 0x43, 0x65, 0x87}, 0x5082EDEEU, 0x2362F9DEU)]
+        [InlineData(new byte[] {0x21, 0x43, 0x65}, 0U, 0x7E4A8634U)]
+        [InlineData(new byte[] {0x21, 0x43}, 0U, 0xA0F7B07AU)]
+        [InlineData(new byte[] {0x21}, 0U, 0x72661CF4U)]
+        [InlineData(new byte[] {0x0, 0x0, 0x0, 0x0}, 0U, 0x2362F9DEU)]
+        [InlineData(new byte[] {0x0, 0x0, 0x0}, 0U, 0x85F0B427U)]
+        [InlineData(new byte[] {0x0, 0x0}, 0U, 0x30F4C306U)]
+        [InlineData(new byte[] {0x0}, 0U, 0x514E28B7U)]
+        public void SmokeTest(byte[] bytes, uint seed, uint expectedHash)
         {
-            MurmurHash3.Hash(new byte[] {1, 2, 3}, 293U).Should().Be(1971362553u);
+            MurmurHash3.Hash32(bytes, seed).Should().Be(expectedHash);
         }
     }
 }
